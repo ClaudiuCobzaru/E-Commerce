739bc08333f6efc1a36127cc4787d660f7f94f61
ClaudiuCobzaru <claudiucobzaru11@gmail.com>
2023-11-17 19:21:13 +0200

    created node server and fetch from backend

---
+++ backend/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Apple Iphone 13',
+      slug: 'apple-iphone-13',
+      category: 'phones',
+      image: '/images/p1.jpg',
+      price: 5000,
+      countInStock: 40,
+      brand: 'Apple',
+      rating: 4.8,
+      numReviews: 79,
+      description: 'noul model de iphone',
+    },
+
+    {
+      name: 'Apple Iphone 13 Pro',
+      slug: 'apple-iphone-13-pro',
+      category: 'phones',
+      image: '/images/p2.jpg',
+      price: 7000,
+      countInStock: 20,
+      brand: 'Apple',
+      rating: 4.6,
+      numReviews: 40,
+      description: 'noul model de iphone',
+    },
+
+    {
+      name: 'Samsung Galxy S22',
+      slug: 'samsung-galaxy-s22',
+      category: 'phones',
+      image: '/images/p3.jpg',
+      price: 4999,
+      countInStock: 90,
+      brand: 'Samsung',
+      rating: 4.4,
+      numReviews: 94,
+      description: 'noul model de samsung',
+    },
+
+    {
+      name: 'Google Pixel 7 Pro',
+      slug: 'google-pixel-7-pro',
+      category: 'phones',
+      image: '/images/p4.jpg',
+      price: 4400,
+      countInStock: 34,
+      brand: 'Google',
+      rating: 4.3,
+      numReviews: 120,
+      description: 'noul model de google pixel',
+    },
+  ],
+};
+
+export default data;
