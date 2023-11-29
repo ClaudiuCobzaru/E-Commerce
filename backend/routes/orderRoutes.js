6d4230c12894ea91b682389674be20c6920044eb
ClaudiuCobzaru <claudiucobzaru11@gmail.com>
2023-11-29 17:27:33 +0100

    implementat place order and place order screen

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import expressAsyncHandeler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+import { isAuth } from '../utils.js';
+
+const orderRouter = express.Router();
+orderRouter.post(
+  '/',
+  isAuth,
+  expressAsyncHandeler(async (req, res) => {
+    const newOrder = new Order({
+      orderItems: req.body.orderItems.map((x) => ({ ...x, product: x._id })),
+      shippingAddress: req.body.shippingAddress,
+      paymentMethod: req.body.paymentMethod,
+      itemsPrice: req.body.itemsPrice,
+      shippingPrice: req.body.shippingPrice,
+      taxPrice: req.body.taxPrice,
+      totalPrice: req.body.totalPrice,
+      user: req.user._id,
+    });
+
+    const order = await newOrder.save();
+    res.status(201).send({ message: 'New Order Created', order });
+  })
+);
+
+orderRouter.get(
+  '/:id',
+  isAuth,
+  expressAsyncHandeler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: ' Order Not Found' });
+    }
+  })
+);
+
+export default orderRouter;
